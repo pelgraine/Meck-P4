f42af78fc048431fb6ef3e925674299cf0f570b1
LILYGO_L <13073925292@163.com>
2025-11-08 09:21:39 +0800

    fix the bug where the sx1262 lora SNR value is not negative

--- main/examples/lvgl_9_ui/lvgl_ui.cpp
+++ main/examples/lvgl_9_ui/lvgl_ui.cpp
@@ -2,7 +2,7 @@
  * @Description: None
  * @Author: LILYGO_L
  * @Date: 2024-11-28 17:07:50
- * @LastEditTime: 2025-10-24 15:52:28
+ * @LastEditTime: 2025-11-07 14:41:06
  * @License: GPL 3.0
  */
 #include "lvgl_ui.h"
@@ -93,7 +93,7 @@ namespace Lvgl_Ui
 #error "unknown macro definition, please select the correct macro definition."
 #endif
 
-            {"firmware build date:\n     ", "202510241555"},
+            {"firmware build date:\n     ", "202511071441"},
     };
 
     void System::begin()
